0df69ac72d020d13fb972030835c562e7bdddf22
kevin knight <tgodkev@gmail.com>
2022-02-18 02:47:35 -0500

    api call set up for 5 day forcst just to set up data.

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
 
-var count = 0;
+const zipBtn = document.getElementById("setZip");
+zipBtn.addEventListener("click", () => setZip() );
 
-const addBtn = document.getElementById("add");
 
 
-addBtn.addEventListener("click", () => handleClick());
 
-function handleClick() {
-  count++;
-  console.log(count);
-  document.getElementById("number").innerHTML = count;
+function setZip(){
+  var zip = document.getElementById("userInput");
+var theZip = zip.value ;
+  currentWeather(theZip);
 }
 
-async function currentWeather(){
-  const response = await fetch('https://api.openweathermap.org/data/2.5/weather?zip=44240&units=imperial&appid=301b282421d5ab0658e1410019293854')
+async function currentWeather(theZip){
+  const response = await fetch('https://api.openweathermap.org/data/2.5/forecast?zip='+ theZip +'&units=imperial&appid=7e58db9063a0da15a6f0a09df1ab8dc5')
   const currentData = await response.json();
   
 
-  document.getElementById("currentTemp").innerHTML = currentData.main.temp;
-  document.getElementById("windSpeed").innerHTML = currentData.wind.speed;
+  document.getElementById("currentTemp").innerHTML = "Current Temp: " + currentData.list[1].main.temp;
+
+  document.getElementById("city").innerHTML = currentData.city.name;
   console.log(currentData)
 }
 
-currentWeather();
 
 
-var zip = document.getElementById("userInput");
-var theZip = zip.value;
 
-console.log(theZip);
+
