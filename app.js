0dffd93fbdf59e7113318e4da7b0c541b60e419a
kevin knight <tgodkev@gmail.com>
2022-02-19 00:06:32 -0500

    day logic added. just needs styled

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ zipBtn.addEventListener("click", () => setZip() );
 
 
 
+
+
 function setZip(){
   var zip = document.getElementById("userInput");
-var theZip = zip.value ;
+  var theZip = zip.value ;
+  const d = new Date();
+  let day = d.getDay();
+
   currentWeather(theZip);
+  setDay(day);
 }
 
 async function currentWeather(theZip){
@@ -17,12 +23,63 @@ async function currentWeather(theZip){
   
 
   document.getElementById("currentTemp").innerHTML = "Current Temp: " + currentData.list[1].main.temp;
-
+  document.getElementById("dayOne").innerHTML =  currentData.list[9].main.temp;
+  document.getElementById("dayTwo").innerHTML =  currentData.list[17].main.temp;
+  document.getElementById("dayThree").innerHTML =  currentData.list[25].main.temp;
+  document.getElementById("dayFour").innerHTML =  currentData.list[33].main.temp;
+  document.getElementById("dayFive").innerHTML =  currentData.list[39].main.temp;
   document.getElementById("city").innerHTML = currentData.city.name;
   console.log(currentData)
 }
 
 
 
+function setDay(day){
+  if (day === 0){
+    
+    document.getElementById("firstDay").innerHTML = 'Sunday';
+    document.getElementById("secondDay").innerHTML = 'Monday';
+    document.getElementById("thirdDay").innerHTML = 'Tuesday';
+    document.getElementById("fourthDay").innerHTML = 'Wedsenday';
+    document.getElementById("fithDay").innerHTML = 'Thursday';
+  }else if(day === 1){
+
+    document.getElementById("firstDay").innerHTML = 'Monday';
+    document.getElementById("secondDay").innerHTML = 'Tuesday';
+    document.getElementById("thirdDay").innerHTML = 'Wedsnday';
+    document.getElementById("fourthDay").innerHTML = 'Thursday';
+    document.getElementById("fithDay").innerHTML = 'Friday';
+  }else if(day === 2){
+    document.getElementById("firstDay").innerHTML = 'Tuesday';
+    document.getElementById("secondDay").innerHTML = 'Wensday';
+    document.getElementById("thirdDay").innerHTML = 'Thursday';
+    document.getElementById("fourthDay").innerHTML = 'Friday';
+    document.getElementById("fithDay").innerHTML = 'Saturday';
+  }else if(day === 3){
+    document.getElementById("firstDay").innerHTML = 'Wednsday';
+    document.getElementById("secondDay").innerHTML = 'Thursday';
+    document.getElementById("thirdDay").innerHTML = 'Friday';
+    document.getElementById("fourthDay").innerHTML = 'Saturday';
+    document.getElementById("fithDay").innerHTML = 'Sunday';
+  }else if(day === 4){
+    document.getElementById("firstDay").innerHTML = 'Thursday';
+    document.getElementById("secondDay").innerHTML = 'Friday';
+    document.getElementById("thirdDay").innerHTML = 'Saturday';
+    document.getElementById("fourthDay").innerHTML = 'Sunday';
+    document.getElementById("fithDay").innerHTML = 'Monday';
+  }else if(day === 5){
+    document.getElementById("firstDay").innerHTML = 'Friday';
+    document.getElementById("secondDay").innerHTML = 'Saturday';
+    document.getElementById("thirdDay").innerHTML = 'Sunday';
+    document.getElementById("fourthDay").innerHTML = 'Monday';
+    document.getElementById("fithDay").innerHTML = 'Tuesday';
+  }else if(day === 6){
+    document.getElementById("firstDay").innerHTML = 'Satuday';
+    document.getElementById("secondDay").innerHTML = 'Sunday';
+    document.getElementById("thirdDay").innerHTML = 'Monday';
+    document.getElementById("fourthDay").innerHTML = 'Tuesday';
+    document.getElementById("fithDay").innerHTML = 'Wednesday';
+  }
+}
 
 
