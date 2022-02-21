152f4b884d6a7b23154f9c4b599e50382a6b4eaf
kevin knight <tgodkev@gmail.com>
2022-02-21 14:15:40 -0500

    styling

--- app.js
+++ app.js
@@ -22,13 +22,13 @@ async function currentWeather(theZip){
   const currentData = await response.json();
   
 
-  document.getElementById("currentTemp").innerHTML = "Current Temp: " + currentData.list[1].main.temp;
+  document.getElementById("currentTemp").innerHTML = "Current Temp: " + currentData.list[1].main.temp + "°F";
   document.getElementById("currentImg").src = "https://openweathermap.org/img/" + currentData.list[1].weather[0].icon + ".png" ;
-  document.getElementById("dayOne").innerHTML =  currentData.list[9].main.temp;
-  document.getElementById("dayTwo").innerHTML =  currentData.list[17].main.temp;
-  document.getElementById("dayThree").innerHTML =  currentData.list[25].main.temp;
-  document.getElementById("dayFour").innerHTML =  currentData.list[33].main.temp;
-  document.getElementById("dayFive").innerHTML =  currentData.list[39].main.temp;
+  document.getElementById("dayOne").innerHTML =  currentData.list[9].main.temp + "°F";
+  document.getElementById("dayTwo").innerHTML =  currentData.list[17].main.temp + "°F";
+  document.getElementById("dayThree").innerHTML =  currentData.list[25].main.temp + "°F";
+  document.getElementById("dayFour").innerHTML =  currentData.list[33].main.temp + "°F";
+  document.getElementById("dayFive").innerHTML =  currentData.list[39].main.temp + "°F";
   document.getElementById("city").innerHTML = currentData.city.name;
   console.log(currentData.list[1])
 }
